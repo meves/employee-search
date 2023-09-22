fae1923ec596cded0ce36b15aabcba060a29bea8
Sergey Medvedkin <meves.sergey@gmail.com>
2023-09-22 09:45:56 +0300

    created app structure

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppRouter from './Router';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <AppRouter/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Content.module.scss'
+
+export const Content = () => {
+    return (
+        <main className={styles.main}>
+            Content
+        </main>
+    )
+}
